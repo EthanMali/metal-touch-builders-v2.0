923c1f3334ba1d3d1042eb5e2728d1f49ad65258
EthanMali <165429414+EthanMali@users.noreply.github.com>
2025-03-20 19:45:09 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
+import GalleryPage from './pages/Gallery';
 
 function App() {
   const location = useLocation();
@@ -22,6 +23,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/gallery" element={<GalleryPage />} />
           </Routes>
         </AnimatePresence>
       </main>
